8625dec43514456281b38e4781f7956994296849
Olivoz <olivoz12345@gmail.com>
2022-11-17 22:30:44 +0100

    Fix forge interact listener

--- forge/src/main/java/io/github/olivoz/snowballing/forge/SnowballingListeners.java
+++ forge/src/main/java/io/github/olivoz/snowballing/forge/SnowballingListeners.java
@@ -5,13 +5,15 @@ import lombok.experimental.UtilityClass;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @UtilityClass
 public final class SnowballingListeners {
 
     @SubscribeEvent
-    public static void onInteract(PlayerInteractEvent event) {
+    public static void onInteract(PlayerInteractEvent.RightClickBlock event) {
+        if(event.getUseItem() == Event.Result.DENY) return;
         Level level = event.getLevel();
         boolean handled = SnowballingInteractionListener.listen(event.getEntity(), level, event.getPos(), event.getItemStack(), SnowballingBlocks.SNOWBALL_PILE.get()::defaultBlockState);
         if(handled) event.setCancellationResult(InteractionResult.sidedSuccess(level.isClientSide));
